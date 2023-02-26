776e7436348457bd2800c7ec7639d99032c4f3d0
Idan <pelled.idan@gmail.com>
2023-02-27 01:53:12 +0300

    add zip and unzip algorithem

--- Client/file.cpp
+++ Client/file.cpp
@@ -1,5 +1,6 @@
 #include "file.h"
 
+#define BUF_SIZE 1024
 #define TIME_FORMAT "%d-%m-%y %H:%M:%S"
 #define FORMAT_LENGTH 18
 
@@ -7,10 +8,10 @@
 extern std::shared_ptr<spdlog::logger> logger;
 
 
-// File info
 bool get_file_information(struct stat& data) {
 	return stat(get_folder_path().c_str(), &data) == 0;
 }
+
 
 const string get_last_modification_date()
 {
@@ -39,76 +40,78 @@ const string get_last_modification_date()
 }
 
 
-
-
-
-
-// File zip
-int get_file_size(const char* path) {
-	HANDLE file = CreateFileA(
-		path, 0, 0, NULL, OPEN_EXISTING,
-		FILE_ATTRIBUTE_NORMAL, NULL
-	);
-
-	long size = GetFileSize(file, NULL);
-	CloseHandle(file);
-
-	return size;
-}
-
-void get_compressor(COMPRESSOR_HANDLE& file) {
-	CreateCompressor(
-		COMPRESS_ALGORITHM_XPRESS_HUFF,
-		NULL, &file
-	);
-}
-
-size_t get_buffer_max_size(const char* buffer, size_t buffer_length) {
-	size_t max_size;
-	COMPRESSOR_HANDLE file;
-
-	get_compressor(file);
-	Compress(
-		file, buffer, buffer_length,
-		NULL, 0,
-		(SIZE_T*) &max_size
-	);
-
-	return max_size;
-}
-
-bool zip_folder(
-	const char* in_buffer,
-	char* out_buffer,
-	size_t& out_data_length
-) {
-	COMPRESSOR_HANDLE file;
-	size_t max_size;
-	bool res;
-
-	get_compressor(file);
-	res = Compress(
-		file, in_buffer, strlen(in_buffer),
-		out_buffer, strlen(out_buffer),
-		(SIZE_T*) &out_data_length
-	);
-	CloseCompressor(file);
-
-	return res;
-}
-
-//TODO:
-bool zip(
-	const char* in_buffer,
-	char* out_buffer,
-	size_t& out_data_length
-) {
-	const char* str = "ZIPED_DIR";
-	memcpy(out_buffer, str, strlen(str));
-	out_data_length = strlen(str);
-	return true;
-}
-
 bool save_folder(const char* folder) {
 	return true;
 }
+
+
+void add_directory_to_zip(mz_zip_archive* zip_archive, const char* path) {
+    mz_zip_writer_add_path(zip_archive, path, NULL, MZ_BEST_COMPRESSION);
+    char search_path[BUF_SIZE];
+    snprintf(search_path, BUF_SIZE, "%s/*", path);
+    void* dir_handle = mz_os_open_dir(search_path);
+    if (dir_handle) {
+        mz_dir_entry dir_entry;
+        while (mz_os_read_dir(dir_handle, &dir_entry)) {
+            if (dir_entry.m_is_dir && strcmp(dir_entry.m_filename, ".") != 0 && strcmp(dir_entry.m_filename, "..") != 0) {
+                char sub_path[BUF_SIZE];
+                snprintf(sub_path, BUF_SIZE, "%s/%s", path, dir_entry.m_filename);
+                add_directory_to_zip(zip_archive, sub_path);
+            }
+            else if (!dir_entry.m_is_dir) {
+                char file_path[BUF_SIZE];
+                snprintf(file_path, BUF_SIZE, "%s/%s", path, dir_entry.m_filename);
+                mz_zip_writer_add_file(zip_archive, dir_entry.m_filename, file_path, NULL, 0, MZ_BEST_COMPRESSION);
+            }
+        }
+        mz_os_close_dir(dir_handle);
+    }
+}
+
+
+char* compress_folder(const char* folder_path, size_t* zip_size) {
+    mz_zip_archive zip_archive;
+    memset(&zip_archive, 0, sizeof(zip_archive));
+    if (!mz_zip_writer_init_heap(&zip_archive, 0)) {
+        return NULL;
+    }
+    add_directory_to_zip(&zip_archive, folder_path);
+    void* zip_data = mz_zip_writer_finalize_heap(&zip_archive, zip_size);
+    char* buffer = new char[*zip_size];
+    memcpy(buffer, zip_data, *zip_size);
+    mz_zip_writer_end(&zip_archive);
+    return buffer;
+}
+
+
+void extract_zip(const char* zip_buffer, size_t zip_size, const char* directory) {
+    mz_zip_archive zip_archive;
+    memset(&zip_archive, 0, sizeof(zip_archive));
+    if (!mz_zip_reader_init_mem(&zip_archive, zip_buffer, zip_size, 0)) {
+        return;
+    }
+    for (int i = 0; i < mz_zip_reader_get_num_files(&zip_archive); i++) {
+        mz_zip_archive_file_stat file_stat;
+        if (!mz_zip_reader_file_stat(&zip_archive, i, &file_stat)) {
+            continue;
+        }
+        char* path = new char[BUF_SIZE];
+        snprintf(path, BUF_SIZE, "%s/%s", directory, file_stat.m_filename);
+        if (file_stat.m_is_directory) {
+            mz_dir_make(path);
+        }
+        else {
+            void* file_handle = mz_os_fopen(path, "wb");
+            if (file_handle) {
+                mz_zip_reader_extract_to_callback(&zip_archive, i, [](void* pOpaque, mz_uint64 file_ofs, const void* pBuf, size_t n) {
+                    mz_os_fwrite(pBuf, n, 1, pOpaque);
+                    return true;
+                    }, file_handle, 0);
+                mz_os_fclose(file_handle);
+            }
+        }
+        delete[] path;
+    }
+    mz_zip_reader_end(&zip_archive);
+}
+
