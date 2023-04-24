8eb79deb7e4e4497cc89e942eeac64f2b56fa86c
IdanPelled <pelled.idan@gmail.com>
2023-04-24 16:25:22 +0300

    edit interface

--- Client/UI/interface.py
+++ Client/UI/interface.py
@@ -3,12 +3,18 @@ import os
 
 def _login(username, password):
     exe = os.getenv("FileSyncPath")
-    cmd = f"{exe} -l -u {username} -p {password}"
-    return os.system(cmd) == 0
+    if exe:
+        cmd = f"{exe} -l -u {username} -p {password}"
+        return os.system(cmd) == 0
+    
+    raise EnvironmentError("missing FileSyncPath env variable")
 
 
 def _signup(username, password):
     exe = os.getenv("FileSyncPath")
-    cmd = f"{exe} -s -u {username} -p {password}"
-    print(cmd)
-    return os.system(cmd) == 0
+    if exe:
+        cmd = f"{exe} -s -u {username} -p {password}"
+        return os.system(cmd) == 0
+
+    raise EnvironmentError("missing FileSyncPath env variable")
+
